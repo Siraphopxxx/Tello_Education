7c7aff8f53f5356b807e1b69b356ffb586f0961b
Siraphopxxx <159603844+Siraphopxxx@users.noreply.github.com>
2024-11-15 15:06:33 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from Tello_Education import Drone
+
+drone = Drone()
+
+# drone.takeoff()
+
+# drone.fly_right(40)
+# drone.fly_left(40) 
+
+# drone.flip_left()
+# # drone.flip_right()
+# # drone.flip_back()
+# # drone.flip_forward()
+
+# drone.fly_back(40)
+# drone.fly_forward(40)
+# drone.fly_up(40)
+# drone.fly_down(40)
+  
+# drone.yaw_left(180)
+# drone.yaw_right(180)
+
+# drone.fly_xyz_speed(40,40,40,50)
+
+# drone.land()
+
+
+#drone.Control()
+drone.Detect()
